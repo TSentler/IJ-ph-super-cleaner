204d7768b8758f944c5e2aa26b4f33096ed33192
TSentner <centner_dc@mail.ru>
2022-09-25 19:53:50 +0500

    Fix background mute

--- Assets/Scripts/YaVk/BackgroundAudioMuteTracker.cs
+++ Assets/Scripts/YaVk/BackgroundAudioMuteTracker.cs
@@ -11,12 +11,14 @@ namespace YaVk
         private void OnEnable()
         {
             _socialNetwork.OnAdsStart += AdsStartHandler;
+            _socialNetwork.OnAdsEnd += AdsEndHandler;
             WebApplication.InBackgroundChangeEvent += OnInBackgroundChange;
         }
 
         private void OnDisable()
         {
-            _socialNetwork.OnAdsEnd += AdsEndHandler;
+            _socialNetwork.OnAdsStart -= AdsStartHandler;
+            _socialNetwork.OnAdsEnd -= AdsEndHandler;
             WebApplication.InBackgroundChangeEvent -= OnInBackgroundChange;
         }
 
@@ -30,12 +32,12 @@ namespace YaVk
             OnInBackgroundChange(false);
         }
 
-        private void OnInBackgroundChange(bool inBackground)
+        private void OnInBackgroundChange(bool isMute)
         {
             // Use both pause and volume muting methods at the same time.
             // They're both broken in Web, but work perfect together. Trust me on this.
-            AudioListener.pause = inBackground;
-            AudioListener.volume = inBackground ? 0f : 1f;
+            AudioListener.pause = isMute;
+            AudioListener.volume = isMute ? 0f : 1f;
         }
     }
 }
