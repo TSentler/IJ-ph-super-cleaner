df2e965e4f894f42ca5c8025d5e31a3667b10418
TSentner <centner_dc@mail.ru>
2022-09-27 17:10:02 +0500

    Add first level 10 sec delay

--- Assets/Scripts/LevelLoader/Saves/FirstLevelLoader.cs
+++ Assets/Scripts/LevelLoader/Saves/FirstLevelLoader.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using Saves;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace LevelLoader
 {
@@ -12,11 +13,15 @@ namespace LevelLoader
         private LevelInfo _levelInfo;
 
         [SerializeField] private GameSaver _saver;
+        [SerializeField] private Slider _slider;
+        [Min(0f), SerializeField] private float _delay = 10f;
 
         private void OnValidate()
         {
             if (_saver == null)
                 Debug.LogWarning("GameSaver was not found!", this);
+            if (_slider == null)
+                Debug.LogWarning("Slider was not found!", this);
         }
 
         private void Awake()
@@ -31,6 +36,14 @@ namespace LevelLoader
             if (lastLevel > -1)
             {
                 levelName = _levelInfo.GetName(lastLevel);
+            }
+
+            var ellapsed = 0f;
+            while (ellapsed < _delay)
+            {
+                ellapsed += 0.5f;
+                _slider.value = ellapsed / _delay;
+                yield return new WaitForSecondsRealtime(0.5f);
             }
             
             SceneManager.LoadScene(levelName);
